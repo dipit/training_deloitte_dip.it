6a88719e0985ce3fa3440dd25171ebf97d4b87f5
dipit sharma <dipitsharma14@gmail.com>
2018-07-27 18:58:39 +0530

    hey

---
+++ assignment2_3/src/assignment2_3/Addition.java
@@ -0,0 +1,20 @@
+package assignment2_3;
+
+
+public class Addition extends Arthimetic {
+	
+	
+
+	public Addition(int n1, int n2,int n3) {
+		super(n1, n2, n3);
+
+	}
+
+	@Override
+	void calculate() {
+		
+		int num3 =  num1+num2;
+		
+	}
+
+}
